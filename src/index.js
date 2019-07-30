c48836697e153e7eca82ae5b96863b73def96378
Paravozik_Misha <witty.html@gmail.com>
2019-07-30 13:48:53 +0300

    init commit "Hello my friend" message

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+class Hello extends React.Component {  
+  render() {  
+      return <h1>Hello world!</h1>;  
+  }  
+}
+
+ReactDOM.render(  
+  <Hello />,   
+  document.getElementById("root")  
+);
