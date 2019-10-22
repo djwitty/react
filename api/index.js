7e29ebedc6a504e81ea5cc241003099071533ed1
Michael aka DjWitty <michael.pov.it@gmail.com>
2019-10-22 21:18:47 +0300

    don't work corrent

--- api/index.js
+++ api/index.js
@@ -2,9 +2,22 @@ import express from 'express';
 import data from '../src/data/testData';
 
 const router = express.Router();
+const contests = data.contests.reduce((obj, contest) => {
+    obj[contest.id] = contest;
+    return obj;
+}, {});
 
 router.get('/contests', (req, res) => {
-    res.send({ contests: data.contests });
+    res.send({
+      contests: contests
+    });
+});
+
+router.get('/contests/:contestId', (req, res) => {
+    let contest = contests[req.params.contestId];
+    contest.description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum doloribus voluptates alias aspernatur, et voluptate nesciunt quis excepturi, aut rem atque. Nesciunt est, voluptate facilis molestias iste consequuntur consequatur?'
+
+    res.send({ contest });
 });
 
 export default router;
--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 //import axios from 'axios';
 import Header from './Header';
 import Footer from './Footer';
-import ContestPreview from './ContestPreview';
+//import ContestPreview from './ContestPreview';
 //import data from '../data/testData';
+import ContestList from './ContestList';
+import Contest from './Contest';
+import * as api from '../api';
+
+const pushState = (obj, url) =>
+  window.history.pushState(obj, '', url);
 
 class App extends React.Component {
   // constructor(props) {
@@ -30,23 +36,55 @@ class App extends React.Component {
     // ajax...
   };
   componentWillUnmount() {};
+  fetchContest = (contestId) => {
+    pushState(
+      { currentContestId: contestId },
+      `/contest/${contestId}`
+    );
+    api.fetchContest(contestId).then(contest => {
+      this.setState({
+        pageHeader: contest.contestName,
+        currentContestId: contest.id,
+        contests: {
+          ...this.state.contests,
+          [contest.id]: contest,
+        }
+      });
+    });
+  };
+  currentContest() {
+    return this.state.contests[this.state.currentContestId];
+  }
+  currentContent() {
+    if (this.state.currentContestId) {
+      return <Contest
+        {...this.currentContest()} />;
+    } else {
+        return <ContestList
+        onContestClick={this.fetchContest}
+        contests={this.state.contests} />;
+    }
+      
+  }
   render() {
     return(
         <div className="App">
-          <Header message={this.state.pageHeader}/>
-          
-          <div className="content">
-            {//this.props.contests.map(contest =>
-              this.state.contests.map(contest =>
-              <ContestPreview key={contest.id} {...contest} />
-            )}
-          </div>
-
+          <Header message={this.state.pageHeader} />
+          {this.currentContent()}
           <Footer message={this.state.pageFooter} />
         </div>
     );
   }
 };
+
+/*
+<ContestList onContestClick={this.fetchContest} contests={this.state.contests} />
+<div className="content">
+  {//this.props.contests.map(contest =>
+    this.state.contests.map(contest =>
+    <ContestPreview key={contest.id} {...contest} />
+  )}
+</div>*/
 
 App.defaultProps = {
   headerMessage: 'I am default message, edit me'
